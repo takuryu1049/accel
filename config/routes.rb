83a6b87f40e6f317e49080418612de25863d32c2
takur-ryu <takuryu.1049@icloud.com>
2020-10-30 02:21:01 +0900

    ルーティング記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :workers
-  devise_for :companies
+  devise_for :workers, controllers: {
+    sessions:      'workers/sessions',
+    passwords:     'workers/passwords',
+    registrations: 'workers/registrations'
+  }
+  devise_for :companies, controllers: {
+    sessions:      'companies/sessions',
+    passwords:     'companies/passwords',
+    registrations: 'companies/registrations'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

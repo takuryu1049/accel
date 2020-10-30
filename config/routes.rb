3c61e84488d67dcb10438a3fc4de6051c76aeb1f
takur-ryu <takuryu.1049@icloud.com>
2020-10-31 00:38:05 +0900

    トップページルーティング記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :workers, controllers: {
-    sessions:      'workers/sessions',
-    passwords:     'workers/passwords',
-    registrations: 'workers/registrations'
-  }
+  root to: "tops#index"
+  get  "/"  => "tops#index"
+
   devise_for :companies, controllers: {
     sessions:      'companies/sessions',
     passwords:     'companies/passwords',
     registrations: 'companies/registrations'
+  }
+  devise_for :workers, controllers: {
+    sessions:      'workers/sessions',
+    passwords:     'workers/passwords',
+    registrations: 'workers/registrations'
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

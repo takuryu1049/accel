6287fa731a617937c080866d851eb6fc29d56a5e
takur-ryu <takuryu.1049@icloud.com>
2020-10-31 10:58:24 +0900

    画像添付機能実装

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,6 @@
 class Company < ApplicationRecord
   has_many :workers
+  has_one_attached :image
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,

5fc63b97e2a2bf150b8d78e645f026dc92c16e66
takur-ryu <takuryu.1049@icloud.com>
2020-10-31 11:43:02 +0900

    会社登録バリデーション設定

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,18 @@
 class Company < ApplicationRecord
   has_many :workers
   has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :image
+    validates :company_login_id
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :city
+    validates :street
+  end
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,

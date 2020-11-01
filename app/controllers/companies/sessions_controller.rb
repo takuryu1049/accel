4fba9c2ec7a25ef98e1f26f93f076e70af265089
takur-ryu <takuryu.1049@icloud.com>
2020-11-01 17:29:02 +0900

    会社ログインコントローラー　ストロングパラメーターの記述

--- app/controllers/companies/sessions_controller.rb
+++ app/controllers/companies/sessions_controller.rb
@@ -1,17 +1,16 @@
 # frozen_string_literal: true
 
 class Companies::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
-
+  before_action :configure_sign_in_params, only: [:create]
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    super
+  end
 
   # DELETE /resource/sign_out
   # def destroy
@@ -19,9 +18,10 @@ class Companies::SessionsController < Devise::SessionsController
   # end
 
   # protected
+  private
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:company_login_id,:password,:password_confirmation])
+  end
 end

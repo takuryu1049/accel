539e7ccae6b28bf4b5cf96bc213fef2e3af43b88
takur-ryu <takuryu.1049@icloud.com>
2020-11-06 15:16:59 +0900

    会社・社員モデル　(バリデーションエラーメッセージ修正)

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -11,14 +11,14 @@ class Worker < ApplicationRecord
          :recoverable, :rememberable, :validatable, authentication_keys: [:worker_login_id]
 
   with_options presence: true do
-    validates :worker_login_id, format: { with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,12}\z/, message: 'は半角6~12文字かつ英大文字・小文字・数字それぞれ１文字以上を含む必要があります' }, uniqueness: true
+    validates :worker_login_id, format: { with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,12}\z/, message: 'は半角6~12文字かつ英大文字・小文字・数字それぞれ１文字以上を含むように入力して下さい' }, uniqueness: true
     validates :image
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角ひらがな、全角カタカナ、漢字で入力必要(名前(全角)/苗字)' }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角ひらがな、全角カタカナ、漢字で入力必要(名前(全角)/名前)' }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナで入力必要(名前カナ(全角)/苗字)' }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナで入力必要(名前カナ(全角)/名前)' }
-    validates :character_id, numericality: { other_than: 0, message: "can't be blank" }
-    validates :position_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角ひらがな、全角カタカナ、漢字のいずれかで入力してください' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角ひらがな、全角カタカナ、漢字でのいずれかで入力してください' }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください' }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください' }
+    validates :character_id, numericality: { other_than: 0, message: "を選択してください" }
+    validates :position_id, numericality: { other_than: 0, message: "を選択してください" }
     validates :born
     validates :email, uniqueness: true
   end

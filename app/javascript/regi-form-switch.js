d155ab17c537afe4767ba89203b6eaef3e1b00ab
takur-ryu <takuryu.1049@icloud.com>
2020-11-10 15:07:49 +0900

    個人・法人所有者情報入力フォーム切り替えスイッチJS作成

---
+++ app/javascript/regi-form-switch.js
@@ -0,0 +1,55 @@
+// document.addEventListener('DOMContentLoaded', function(){
+//   const CompanyName = document.getElementById('company-name');
+//   console.log("読み込まれています");
+// };
+function RegiFormSwitch() {
+  // 必要な要素を全て取得しておく。
+  const HumanSwitch = document.getElementById('human-switch');
+  const CompanySwitch = document.getElementById('company-switch');
+  const OwnerCompanyName = document.getElementById('owner-company-name');
+  const OwnerCompanyNameInput = document.getElementById('owner-company-name-input');
+  const OwnerLastName = document.getElementById('owner-last-name');
+  const OwnerLastNameInput = document.getElementById('owner-last-name-input');
+  const OwnerFirstName = document.getElementById('owner-first-name');
+  const OwnerFirstNameInput = document.getElementById('owner-first-name-input');
+  const OwnerLastNameKana = document.getElementById('owner-last-name-kana');
+  const OwnerLastNameKanaInput = document.getElementById('owner-last-name-kana-input');
+  const OwnerFirstNameKana = document.getElementById('owner-first-name-kana');
+  const OwnerFirstNameKanaInput = document.getElementById('owner-first-name-kana-input');
+
+  // 個人ラジオボタンに変更された時の挙動
+  HumanSwitch.addEventListener("change", () => {
+    OwnerCompanyNameInput.value = "";
+    OwnerCompanyName.classList.add('animate__animated',"animate__zoomOut");
+    OwnerLastName.classList.remove('animate__animated',"animate__zoomOut","owner-last-name-none");
+    OwnerFirstName.classList.remove('animate__animated',"animate__zoomOut","owner-first-name-none");
+    OwnerLastNameKana .classList.remove('animate__animated',"animate__zoomOut","owner-last-name-kana-none");
+    OwnerFirstNameKana .classList.remove('animate__animated',"animate__zoomOut","owner-first-name-kana-none");
+
+    setTimeout(() => {
+      OwnerCompanyName.classList.add("owner-company-name-none");
+    }, 130)
+  });
+
+  // 法人ラジオボタンに変更された時の挙動
+  CompanySwitch.addEventListener("change", () => {
+    OwnerLastNameInput.value = "";
+    OwnerLastName.classList.add('animate__animated',"animate__zoomOut");
+    OwnerFirstNameInput.value = "";
+    OwnerFirstName.classList.add('animate__animated',"animate__zoomOut");
+    OwnerLastNameKanaInput.value = "";
+    OwnerLastNameKana.classList.add('animate__animated',"animate__zoomOut");
+    OwnerFirstNameKanaInput.value = "";
+    OwnerFirstNameKana.classList.add('animate__animated',"animate__zoomOut");
+    OwnerCompanyName.classList.remove('animate__animated',"animate__zoomOut","owner-company-name-none");
+
+
+    setTimeout(() => {
+      OwnerLastName.classList.add("owner-last-name-none");
+      OwnerFirstName.classList.add("owner-first-name-none");
+      OwnerLastNameKana.classList.add("owner-last-name-kana-none");
+      OwnerFirstNameKana.classList.add("owner-first-name-kana-none");
+    }, 130)
+  });
+}
+window.addEventListener('load',RegiFormSwitch);

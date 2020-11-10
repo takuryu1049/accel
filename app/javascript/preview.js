303f5a86136f0c5545ce7a33ef58ac6c95baef87
takur-ryu <takuryu.1049@icloud.com>
2020-11-10 15:06:29 +0900

    物件画像登録プレビューJS作成

---
+++ app/javascript/preview.js
@@ -0,0 +1,38 @@
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+  document.addEventListener('DOMContentLoaded', function(){
+    const ImageList = document.getElementById('image-list');
+    const NoImage = document.getElementById('no-image');
+    const createImageHTML = (blob) => {
+       // 画像を表示するためのdiv要素を生成
+      const imageElement = document.createElement('div');
+
+      // 表示する画像を生成
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('src', blob);
+      blobImage.setAttribute('width', "630");
+      blobImage.setAttribute('height', "490");
+      blobImage.setAttribute('id', "set");
+      // 生成したHTMLの要素をブラウザに表示させる
+      imageElement.appendChild(blobImage);
+      ImageList.appendChild(imageElement);
+    };
+
+    document.getElementById('item-image').addEventListener('change', function(e){
+      // 画像が表示されている場合のみ、すでに存在している画像を削除する
+      ImageList.setAttribute("style", "background-image:none;");
+      const imageContent = document.getElementById('set');
+      if (NoImage){
+        NoImage.remove();
+      }
+      if (imageContent){
+        imageContent.remove();
+      }
+      
+
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+
+      createImageHTML(blob);
+    });
+  });
+}

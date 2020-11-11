0e813652bf3ccff5205f905bbc542cb0c9aa1d46
takur-ryu <takuryu.1049@icloud.com>
2020-11-11 11:25:57 +0900

    propertiesテーブル作成

--- db/migrate/20201110134755_create_properties.rb
+++ db/migrate/20201110134755_create_properties.rb
@@ -1,7 +1,19 @@
 class CreateProperties < ActiveRecord::Migration[6.0]
   def change
     create_table :properties do |t|
-
+      t.string :name, null: false
+      t.string :name_kana, null: false
+      t.string :postal_code, null: false
+      t.integer :prefecture_id, null: false
+      t.string :city, null: false
+      t.string :street, null: false
+      t.integer :type, null: false
+      t.integer :units, null: false
+      t.integer :management_form, null: false
+      t.integer :rank, null: false
+      t.text :cauntion
+      t.references :company, null: false, foreign_key: true
+      t.references :worker, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,73 +10,96 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_201_030_171_541) do
-  create_table 'active_storage_attachments', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'record_type', null: false
-    t.bigint 'record_id', null: false
-    t.bigint 'blob_id', null: false
-    t.datetime 'created_at', null: false
-    t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
-    t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness', unique: true
+ActiveRecord::Schema.define(version: 2020_11_10_134755) do
+
+  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table 'active_storage_blobs', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC', force: :cascade do |t|
-    t.string 'key', null: false
-    t.string 'filename', null: false
-    t.string 'content_type'
-    t.text 'metadata'
-    t.bigint 'byte_size', null: false
-    t.string 'checksum', null: false
-    t.datetime 'created_at', null: false
-    t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
+  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table 'companies', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'company_login_id', null: false
-    t.string 'post_code', null: false
-    t.integer 'prefecture_id', null: false
-    t.string 'city', null: false
-    t.string 'street', null: false
-    t.string 'building_name'
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['company_login_id'], name: 'index_companies_on_company_login_id', unique: true
-    t.index ['email'], name: 'index_companies_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_companies_on_reset_password_token', unique: true
+  create_table "companies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "company_login_id", null: false
+    t.string "post_code", null: false
+    t.integer "prefecture_id", null: false
+    t.string "city", null: false
+    t.string "street", null: false
+    t.string "building_name"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["company_login_id"], name: "index_companies_on_company_login_id", unique: true
+    t.index ["email"], name: "index_companies_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_companies_on_reset_password_token", unique: true
   end
 
-  create_table 'workers', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC', force: :cascade do |t|
-    t.string 'worker_login_id', null: false
-    t.string 'last_name', null: false
-    t.string 'first_name', null: false
-    t.string 'last_name_kana', null: false
-    t.string 'first_name_kana', null: false
-    t.boolean 'gender', default: false, null: false
-    t.date 'born', null: false
-    t.integer 'character_id', null: false
-    t.integer 'position_id', null: false
-    t.integer 'qualification_id'
-    t.bigint 'company_id', null: false
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['company_id'], name: 'index_workers_on_company_id'
-    t.index ['email'], name: 'index_workers_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_workers_on_reset_password_token', unique: true
-    t.index ['worker_login_id'], name: 'index_workers_on_worker_login_id', unique: true
+  create_table "properties", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "name_kana", null: false
+    t.string "postal_code", null: false
+    t.integer "prefecture_id", null: false
+    t.string "city", null: false
+    t.string "street", null: false
+    t.integer "type", null: false
+    t.integer "units", null: false
+    t.integer "management_form", null: false
+    t.integer "rank", null: false
+    t.text "cauntion"
+    t.bigint "company_id", null: false
+    t.bigint "worker_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["company_id"], name: "index_properties_on_company_id"
+    t.index ["worker_id"], name: "index_properties_on_worker_id"
   end
 
-  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
-  add_foreign_key 'workers', 'companies'
+  create_table "workers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC", force: :cascade do |t|
+    t.string "worker_login_id", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.boolean "gender", default: false, null: false
+    t.date "born", null: false
+    t.integer "character_id", null: false
+    t.integer "position_id", null: false
+    t.integer "qualification_id"
+    t.bigint "company_id", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["company_id"], name: "index_workers_on_company_id"
+    t.index ["email"], name: "index_workers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_workers_on_reset_password_token", unique: true
+    t.index ["worker_login_id"], name: "index_workers_on_worker_login_id", unique: true
+  end
+
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "properties", "companies"
+  add_foreign_key "properties", "workers"
+  add_foreign_key "workers", "companies"
 end

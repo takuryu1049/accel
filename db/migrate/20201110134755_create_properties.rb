21aad1dfb20e4fa29dbc5de54ce5a081c66a4188
takur-ryu <takuryu.1049@icloud.com>
2020-11-14 23:28:41 +0900

    物件モデル　カラム名修正・追加

--- db/migrate/20201110134755_create_properties.rb
+++ db/migrate/20201110134755_create_properties.rb
@@ -3,17 +3,18 @@ class CreateProperties < ActiveRecord::Migration[6.0]
     create_table :properties do |t|
       t.string :name, null: false
       t.string :name_kana, null: false
-      t.string :postal_code, null: false
+      t.string :post_code, null: false
       t.integer :prefecture_id, null: false
       t.string :city, null: false
       t.string :street, null: false
-      t.integer :type, null: false
+      t.integer :type_id, null: false
       t.integer :units, null: false
-      t.integer :management_form, null: false
-      t.integer :rank, null: false
-      t.text :cauntion
+      t.integer :management_form_id, null: false
+      t.integer :rank_id, null: false
+      t.text :caution
       t.references :company, null: false, foreign_key: true
       t.references :worker, null: false, foreign_key: true
+      t.references :owner, null: false, foreign_key: true
       t.timestamps
     end
   end

e0170ea6219ef0a0818d54149c1276e0fde6dc03
takur-ryu <takuryu.1049@icloud.com>
2020-11-23 04:23:07 +0900

    オーナーモデル：アクティブハッシュアソシエーション追加

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,4 +1,7 @@
 class Owner < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :character
+  belongs_to_active_hash :prefecture
   has_many :properties
   has_many :relation_persons
   has_many :banks

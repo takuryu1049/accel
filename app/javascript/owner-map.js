c9cd073123bb72f439d0840a4f3e5ec083691cec
takur-ryu <takuryu.1049@icloud.com>
2020-11-23 04:23:35 +0900

    googlemap 大家様用　JS

---
+++ app/javascript/owner-map.js
@@ -0,0 +1,23 @@
+function mapOwner(){
+  let map
+  let geocoder
+  geocoder = new google.maps.Geocoder()
+  map = new google.maps.Map(document.getElementById('map-owner'), {
+  center: {lat: -34.397, lng: 150.644},
+  zoom: 17
+  });
+  let inputAddress = document.getElementById('owner-map-dom').textContent;
+  geocoder.geocode( { 'address': inputAddress}, function(results, status) {
+    if (status == 'OK') {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
+window.addEventListener('load',mapOwner);
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,7 @@ require('../input_pen')
 require('../work_place-switch')
 require('../sort')
 require('../map')
+require('../owner-map')
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.

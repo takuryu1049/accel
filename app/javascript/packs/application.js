773edd1514ecc30adb347f7e82dd24f261acbbf5
Ryuya Takusari <takuryu.1049@icloud.com>
2020-12-01 19:03:16 +0900

    収益リソース　ビュー仮置

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,6 +16,7 @@ require('../map')
 require('../owner-map')
 require('../fullscreen')
 require('../select')
+require('../resource-calc')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)

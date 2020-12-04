86640675204bff5f6acf3b4f6099a569c04e3e66
Ryuya Takusari <takuryu.1049@icloud.com>
2020-12-04 10:43:41 +0900

    resourcesテーブルの作成

--- db/migrate/20201204013809_create_resources.rb
+++ db/migrate/20201204013809_create_resources.rb
@@ -1,7 +1,8 @@
 class CreateResources < ActiveRecord::Migration[6.0]
   def change
     create_table :resources do |t|
-
+      t.integer :brokerage_fee, null: false
+      t.integer :add_fee, null: false
       t.timestamps
     end
   end

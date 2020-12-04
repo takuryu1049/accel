02dfce22a6ae0e0c6c80bef811471c274bbc0bc3
Ryuya Takusari <takuryu.1049@icloud.com>
2020-12-04 15:25:21 +0900

    パスワードマスキング実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,6 +20,7 @@ require('../resource-calc')
 require('../menu')
 require('jquery')
 require('../stalker')
+require('../passoword-mask')
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
---
+++ app/javascript/passoword-mask.js
@@ -0,0 +1,32 @@
+function PassMask() {
+  const PasswordInput = document.querySelector('.password-input');
+  const PasswordConfirmationInput = document.querySelector('.password-confirmation-input');
+  const PasswordEye = document.querySelector('.password-eye');
+  const PasswordConfirmationEye = document.querySelector('.password-confirmation-eye');
+
+  PasswordEye.addEventListener("click", () => {
+    if(PasswordInput.type === "password"){
+      PasswordInput.type = "text";
+      PasswordEye.classList.remove("fa-eye");
+      PasswordEye.classList.add("fa-eye-slash"); 
+    }else{
+      PasswordInput.type = "password";
+      PasswordEye.classList.remove("fa-eye-slash"); 
+      PasswordEye.classList.add("fa-eye");
+    }
+  });
+
+  PasswordConfirmationEye.addEventListener("click", () => {
+    if(PasswordConfirmationInput.type === "password"){
+      PasswordConfirmationInput.type = "text";
+      PasswordConfirmationEye.classList.remove("fa-eye");
+      PasswordConfirmationEye.classList.add("fa-eye-slash"); 
+    }else{
+      PasswordConfirmationInput.type = "password";
+      PasswordConfirmationEye.classList.remove("fa-eye-slash"); 
+      PasswordConfirmationEye.classList.add("fa-eye");
+    }
+  });
+
+}
+window.addEventListener('load',PassMask);

5902dca01caccf25412bf5ba657fd33377ef20d4
Ryuya Takusari <takuryu.1049@icloud.com>
2020-12-12 17:16:06 +0900

    本日登録した物件一覧表示機能

--- app/controllers/app_tops_controller.rb
+++ app/controllers/app_tops_controller.rb
@@ -1,5 +1,6 @@
 class AppTopsController < ApplicationController
   def index
+    @day_of_properties = current_company.properties.where(created_at: Time.now.in_time_zone.all_day)
     @month_of_properties = current_company.properties.where(created_at: Time.now.in_time_zone.all_month)
     @workers = current_company.workers
   end

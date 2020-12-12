32d797482c65aeab421b154340cf884cb4b32184
Ryuya Takusari <takuryu.1049@icloud.com>
2020-12-12 15:54:56 +0900

    在籍リスト表示機能実装

--- app/controllers/app_tops_controller.rb
+++ app/controllers/app_tops_controller.rb
@@ -1,5 +1,6 @@
 class AppTopsController < ApplicationController
   def index
     @month_of_properties = current_company.properties.where(created_at: Time.now.in_time_zone.all_month)
+    @workers = current_company.workers
   end
 end

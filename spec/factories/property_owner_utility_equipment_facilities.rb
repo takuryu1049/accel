c93b392c5a3d60e3766c300ea3dc306922661a41
Ryuya Takusari <takuryu.1049@icloud.com>
2020-12-20 12:30:31 +0900

    factoryボットの修正

--- spec/factories/property_owner_utility_equipment_facilities.rb
+++ spec/factories/property_owner_utility_equipment_facilities.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
   factory :property_owner_utility_equipment_facility do
     
+# コメントアウトされている内容は任意の情報になります。
+
+    #property部分
     name{"#{Gimei.last.kanji}ビル"}
     name_kana{"#{Gimei.last.katakana}ビル"}
     post_code{"#{Faker::Number.number(digits: 3)}-#{Faker::Number.number(digits: 4)}"}
@@ -12,50 +15,41 @@ FactoryBot.define do
     management_form_id{Faker::Number.between(from: 1, to: 7)}
     image{File.open("#{Rails.root}/public/images/test_image.png")}
     rank_id{Faker::Number.between(from: 1, to: 4)}
-    caution{"#{name}は#{["綺麗な","少し古い"].sample}建物です。"}
+
     #utility部分
     water_supply_id{Faker::Number.between(from: 1, to: 2)}
     sewer_id{Faker::Number.between(from: 1, to: 2)}
     electrical_id{Faker::Number.between(from: 1, to: 3)}
     ga_id{Faker::Number.between(from: 1, to: 3)}
 
-    trait :owner_swicth_is_true do
-      swicth_owner_form{true}
+      # 以下、必須ではない任意の値 🔻
+    # caution{"#{name}は#{["綺麗な","少し古い"].sample}建物です。"}
+
+    # 個人の個別情報部分
+    trait :owner_swicth_is_human do
+      swicth_owner_form{"H"}
       last_name{ Gimei.last.kanji }
       first_name{ Gimei.first.kanji }
       last_name_kana{ Gimei.last.katakana }
       first_name_kana{ Gimei.first.katakana }
-      gender{[true,false].sample}
-      character_id{Faker::Number.between(from: 1, to: 3)}
-      character_about{"#{last_name}さんは、優しい方です"}
       main_communication{"本人"}
-      w_company_name{"テスト株式会社"}
-      w_company_name_kana{"テストカブシイガイシャ"}
-      w_post_code{"#{Faker::Number.number(digits: 3)}-#{Faker::Number.number(digits: 4)}"}
-      w_prefecture_id{Faker::Number.between(from: 1, to: 47)}
-      w_city{"#{Gimei.address.city.kanji}#{Gimei.address.town.kanji}"}
-      w_street{"#{Faker::Number.between(from: 1, to: 10)}-#{Faker::Number.between(from: 1, to: 10)}"}
-      w_building_name{"テストビル101"}
-      w_company_phone_num{"000-0000-0000"}
-      w_job_description{"教育事業"}
     end
-    trait :owner_swicth_is_false do
-      swicth_owner_form{false}
+    
+    # 法人の個情報部分 
+    trait :owner_swicth_is_company do
+      swicth_owner_form{"C"}
       owner_company_name{ Gimei.last.kanji+"不動産株式会社" }
       owner_company_name_kana{"#{Gimei.last.katakana}フドウサンカブシキガイシャ"}
-      character_about{"#{owner_company_name}様は、有名な会社です"}
       main_communication{"営業部門の田中様"}
+
+      # セレクトボックスはnilとして認識される為、初期値として0を置く。
+      # ※法人をした場合に、0以外の値を受け付けない為。
+      character_id{0}
     end
     owner_post_code{"#{Faker::Number.number(digits: 3)}-#{Faker::Number.number(digits: 4)}"}
     owner_prefecture_id{Faker::Number.between(from: 1, to: 47)}
     owner_city{"#{Gimei.address.city.kanji}#{Gimei.address.town.kanji}"}
     owner_street{"#{Faker::Number.between(from: 1, to: 10)}-#{Faker::Number.between(from: 1, to: 10)}"}
-    owner_building_name{"アクセルビル101"}
     communication_about{"主に電話"}
-    home_phone_num{"000-0000-0000"}
-    phone_num{"000-0000-0000"}
-    other_phone_num{"000-0000-0000"}
-    fax_num{"000-0000-0000"}
-    email{"test@com"}
   end
 end

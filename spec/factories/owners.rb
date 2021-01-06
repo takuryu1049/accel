18690eb3834950d434fcecc77a3fa0439da58757
Ryuya Takusari <takuryu.1049@icloud.com>
2021-01-06 17:14:54 +0900

    ownerのファクトリーボットを作成

--- spec/factories/owners.rb
+++ spec/factories/owners.rb
@@ -31,5 +31,29 @@
 FactoryBot.define do
   factory :owner do
     
+    # 個人の情報を生成する場合のtrait(特性)
+    trait :owner_swicth_is_human do
+      swicth_owner_form{"H"}
+      last_name{ Gimei.last.kanji }
+      first_name{ Gimei.first.kanji }
+      last_name_kana{ Gimei.last.katakana }
+      first_name_kana{ Gimei.first.katakana }
+      main_communication{"本人"}
+    end
+    
+    # 法人の情報を生成する場合のtrait(特性)
+    trait :owner_swicth_is_company do
+      swicth_owner_form{"C"}
+      company_name{ Gimei.last.kanji+"不動産株式会社" }
+      company_name_kana{"#{Gimei.last.katakana}フドウサンカブシキガイシャ"}
+      main_communication{"営業部門の田中様"}
+    end
+
+    post_code{"#{Faker::Number.number(digits: 3)}-#{Faker::Number.number(digits: 4)}"}
+    prefecture_id{Faker::Number.between(from: 1, to: 47)}
+    city{"#{Gimei.address.city.kanji}#{Gimei.address.town.kanji}"}
+    street{"#{Faker::Number.between(from: 1, to: 10)}-#{Faker::Number.between(from: 1, to: 10)}"}
+    communication_about{"主に電話"}
+
   end
 end
